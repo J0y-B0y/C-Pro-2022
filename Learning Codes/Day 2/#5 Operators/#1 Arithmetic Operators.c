8f5f03d018fde5a12b3a89e520d5ff3918769d3a
Divjot Singh <98224512+J0y-B0y@users.noreply.github.com>
2022-06-25 20:23:14 +0530

    Using Basic Arithmetic Operations

---
+++ Learning Codes/Day 2/#5 Operators/#1 Arithmetic Operators.c
@@ -0,0 +1,16 @@
+// Case 1 [a, b are integers]
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    a = 34;
+    b = 6;
+
+    printf("a + b = %d\n", a+b);
+    printf("a - b = %d\n", a-b);
+    printf("a * b = %d\n", a*b);
+    printf("a / b = %d\n", a/b);
+
+    return 0;
+}
